ea0f34a57ced9d9e85c3e7eb04939281cc9c9a60
Boomb0x <Ghost2130@gmail.com>
2021-06-12 13:30:05 -0400

    change to pokedex api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,32 @@ var zipFormEl = document.querySelector('#zip-form');
 var zipInputEl = document.querySelector('#Zip');
 var todayBox = document.querySelector('#today-box');
 var menuList = document.querySelector('#menu-list');
+var abilities
+var moves
+var stats
+var item
 
 
 var zipForm = function (event){
     event.preventDefault();
 
-    var zip = zipInputEl.value.trim();
-    if(zip) {
-        getTodayWeather(zip);
-        getAreaMenu(zip);
-        console.log(zip);
+    var pokemon = zipInputEl.value.trim();
+    if(pokemon) {
 
-        // todayBox.textContent = '';
-        // zipInputEl.value = '';
+        getTodayWeather(pokemon);
+        hotelDisplay(pokemon);
+        Stats(pokemon);
+        console.log(pokemon);
+
     } else {
-        alert('Please enter a zip code')
+        alert('Please enter a generation 8 Overused pokemon and check spelling')
     }
 };
 
 
 
-var getTodayWeather = function (zip) {
-    var todayApi = 'https://api.openweathermap.org/data/2.5/weather?zip='+zip+'&units=imperial&appid=8066018655ed008933d2f7b865448329';
+var getTodayWeather = function (pokemon) {
+    var todayApi = 'https://pokeapi.co/api/v2/pokemon-species/'+pokemon+'/';
 
     fetch(todayApi)
         .then(function (response) {
@@ -33,37 +37,104 @@ var getTodayWeather = function (zip) {
                 response.json().then(function (data) {
                     console.log(data);
                     
+                    const entry = data.flavor_text_entries.find(fte=>fte.language.name ==='en')
+                    console.log(entry)
+
+                    
                 })
             }
         })
 }
 
-var getAreaMenu = function (zip) {
-    var menuApi = 'https://api.documenu.com/v2/restaurants/zip_code/'+zip+'?key=a066d36aaebc3eeceb6e2edd2c821cbb&fullmenu=true&size=50&page=1';
+var Stats = function (pokemon) {
+    var baseStats = 'https://pokeapi.co/api/v2/pokemon/'+pokemon+'/';
 
-    fetch(menuApi)
+    fetch(baseStats)
         .then(function (response) {
             if (response.ok) {
                 console.log(response);
                 response.json().then(function (data) {
                     console.log(data);
 
+                var hp = data.stats[0].base_stat;
+                var attack = data.stats[1].base_stat;
+                var defense = data.stats[2].base_stat;
+                var special_att = data.stats[3].base_stat;
+                var special_def = data.stats[4].base_stat;
+                var speed = data.stats[5].base_stat;
+                
+                console.log('HP',hp);
+                console.log('attack', attack);
+                console.log('defense', defense);
+                console.log('specialattack', special_att);
+                console.log('special_def', special_def);
+                console.log('speed', speed);
 
-                    for (var i = 0; i < 10; i++) {
-                        var name = data.data[i].restaurant_name;
-                        var address = data.data[i].address.formatted;
-                       var  phone = data.data[i].restaurant_phone;
-                       var website = data.data[i].restaurant_website;
+                for (var i = 0; i < data.types.length; i++) {
+                    var type = data.types[i].type;
+                    console.log('type',type)
+                }
 
-                       console.log('name',name);
-                       console.log('address',address);
-                       console.log('phone',phone);
-                       console.log('website',website);
-                    }
-                    
                 })
             }
         })
+}
+
+// var getAreaMenu = function (zip) {
+//     var menuApi = 'https://api.documenu.com/v2/restaurants/zip_code/'+zip+'?key=a066d36aaebc3eeceb6e2edd2c821cbb&fullmenu=true&size=50&page=1';
+
+//     fetch(menuApi)
+//         .then(function (response) {
+//             if (response.ok) {
+//                 console.log(response);
+//                 response.json().then(function (data) {
+//                     console.log(data);
+//                     var cityName = data.data[0].address.city
+//                     console.log(cityName);
+
+//                     hotelDisplay(cityName);
+
+//                     for (var i = 0; i < 10; i++) {
+//                         var name = data.data[i].restaurant_name;
+//                         var address = data.data[i].address.formatted;
+//                        var  phone = data.data[i].restaurant_phone;
+//                        var website = data.data[i].restaurant_website;
+                       
+
+//                        console.log('name',name);
+//                        console.log('address',address);
+//                        console.log('phone',phone);
+//                        console.log('website',website);
+//                     }
+                    
+//                 })
+//             }
+//         })
+// }
+
+function hotelDisplay(pokemon) {
+    var requestUrl = 'https://cors-anywhere.herokuapp.com/https://smogon-usage-stats.herokuapp.com/gen8ou/'+pokemon;
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+
+            var ability = data.abilities;
+            var items = data.items;
+            var moves = data.moves;
+            var spreads = data.spreads;
+
+            console.log("ability", ability);
+            console.log('items',items);
+            console.log('moves',moves);
+            console.log('spreads',spreads)
+        })
+
+        
+
 }
 
 
